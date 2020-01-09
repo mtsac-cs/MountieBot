d3bde00c47aabbfeb8d7baab459e7a5c32a8b1ae
Faizaan <fizz321@gmail.com>
2020-01-09 14:31:44 -0800

    Added Weather Command
    Added a weather command to Info cog. Gets weather for any city.

--- cogs/info.py
+++ cogs/info.py
@@ -3,7 +3,8 @@ from discord.ext import commands
 
 
 from PyDictionary import PyDictionary
-
+from bs4 import BeautifulSoup
+import requests
 
 dictionary=PyDictionary()
 
@@ -21,6 +22,34 @@ class Info(commands.Cog):
             await ctx.send(key + ":")
             for definition in obj[key]:
                 await ctx.send(definition)
+
+    @commands.command()
+    async def weather(self, ctx, arg):
+        """ - Gives weather for any city"""
+        url = "https://www.google.com/search?q=weather+in+" + arg
+        headers_Get = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,/;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1'
+        }
+
+        page = requests.get(url, headers=headers_Get)
+
+        soup = BeautifulSoup(page.text, 'html.parser')
+        city = soup.find(id="wob_loc").get_text()
+        time = soup.find(id="wob_dts").get_text()
+        weatherState = soup.find(id="wob_dc").get_text()
+        temp = soup.find(id='wob_tm').get_text()
+        precip = soup.find(id='wob_pp').get_text()
+        humid = soup.find(id='wob_hm').get_text()
+        wind = soup.find(id='wob_ws').get_text()
+
+        await ctx.send("Weather in " + city + "\n" + time + "\n" + weatherState + "\n" + "->Temperature: "+ temp + "°F \n" + "->Precipitation: " + precip + "\n" + "->Humidity: " + humid + "\n" + "->Wind: " + wind + "\n")
+
 
 def setup(client):
     client.add_cog(Info(client))
